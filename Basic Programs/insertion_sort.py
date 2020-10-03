fd99f2925c9f707be0ba41b79d5d7da85fb64cbd
Punnatt Punnotok <punnattnp@Punnatts-MacBook-Pro.local>
2020-10-03 21:54:45 +0700

    Insertion sort in Python

---
+++ Basic Programs/insertion_sort.py
@@ -0,0 +1,13 @@
+def insertion_sort(array): 
+    for i in range(1, len(array)): 
+        key = array[i] 
+        j = i-1
+        while j >=0 and key < array[j] : 
+                array[j+1] = array[j] 
+                j -= 1
+        array[j+1] = key 
+  
+numbers = [9,6,5,1,5,3,6,7]
+insertion_sort(numbers)
+for i in numbers:
+        print(i)
